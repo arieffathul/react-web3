1087c48a33117c32d3b17adc979ae497bfd4089b
arieffathul <ariefulum2005@gmail.com>
2024-11-13 21:30:21 +0700

    Memperbaiki error deploy

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
 import { Outlet } from "react-router-dom";
-import Navbar from "../component/navbar";
+import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
 import { ThemeProvider } from "../context/ThemeContext";
 
